d5e48f88b2ec54ae93dfcf376f3b841452fa1760
larchambault <36712660+larchambault@users.noreply.github.com>
2018-12-12 18:14:47 -0500

    initial commit

---
+++ gamma3D.py
@@ -0,0 +1,161 @@
+'''
+Created on 2018-12-06
+
+@author: Louis Archambault
+'''
+
+import numpy as np
+from scipy.ndimage.morphology import distance_transform_edt as edt
+
+class gamma:
+   """ Gamma calculation (global) based on Chen et al. 2009
+
+       WARNING (for now): assume same sampling between ref and meas
+       Compute one distance map then apply to multiple measurements
+       see reading notes and jupyter_notebook/gamma
+   """
+
+
+   def __init__(self,dose_crit,dist_crit,vox_size = None,ref=None):
+      """ initialization
+
+          ref: reference dose distribution a 3D numpy array
+          vox size: voxel size [x,y,z] in mm
+          dose_crit: \Delta [percent of max]
+          dist_crit: \delta [mm]
+      """
+
+      self.dist_crit = dist_crit
+      self.dose_crit = dose_crit
+      if vox_size is not None:
+         self.set_voxel_size(vox_size)
+      else:
+         self.set_voxel_size([1,1,1])
+      self.max_dose_pct = 120 # [%], fraction of maximum to use as the upper dose bin
+
+      # empty initialization, waiting for ref image
+      self.ndbins = None # number of dose bins
+      self.dbins = []
+      self.ref_img = None
+      self.dist_map = None
+      self.delta = None # voxel sizes as fx of criterion
+
+
+      if ref is not None:
+         self.set_reference(ref)
+
+   def set_voxel_size(self,vox_size):
+      self.vox_size = np.array(vox_size) # calling np.array does nothing if vox_size is already a ndarray
+
+   def set_reference(self,ref):
+
+      # dx -> x,y,z distance as fx of distance criterion
+      dx = self.vox_size/self.dist_crit # voxel dimensions expressed as fx of criterion
+      dd = np.mean(dx) # dimension along dose axis should be the same as space axis. Dose bins are set afterward
+
+      # absolute dose criterion = % of max
+      max_dose =  np.max(ref)
+      abs_dose_crit = max_dose*self.dose_crit/100.
+
+      # Set the number of dose bins to match the voxel dose length (i.e. dd)
+      # dd = dose_vox_size/abs_dose_crit
+      # dose_vox_size = max_dose/ndbins
+      # max_dose = (maximum of ref)*max_dose_pct
+      # -> ndbins = max_dose/(dd * abs_dose_crit)
+      ndbins = int(np.ceil(max_dose*self.max_dose_pct/100/dd/abs_dose_crit))
+      dbins = np.linspace(0,max_dose*self.max_dose_pct/100,ndbins+1)
+
+      # assign important data to self
+      self.ref_img = ref
+      self.delta = np.append(dx,dd) # voxel sizes as fx of criterion
+      self.ndbins = ndbins
+      self.dbins = dbins
+
+
+      self.compute_dist_map()
+
+   def compute_dist_map(self):
+      """ Compute distance map (can be time consuming) """
+      # Build an hyper surface with three spatial dimensions + 1 dose dimension
+      hypSurfDim = self.ref_img.shape + (self.ndbins,)
+      hypSurf = np.ones( hypSurfDim )
+
+      # Fill each layer of the dose axis
+      # Dose points are set to 0
+
+      lookup = np.digitize(self.ref_img,self.dbins) - 1 # lookup contains the index of dose bins
+      for i in range(self.ndbins):
+         dose_points = lookup == i
+         hypSurf[:,:,:,i][dose_points] = 0
+
+      # simple (naive) interpolation. See Fig. 2 au Chen 2009
+      hypSurf = self._interp_dose_along_ax(hypSurf,lookup,0)
+      hypSurf = self._interp_dose_along_ax(hypSurf,lookup,1)
+      hypSurf = self._interp_dose_along_ax(hypSurf,lookup,2)
+
+      # print(self.delta)
+      dst = edt(hypSurf,sampling=self.delta)
+
+      self.dist_map = dst
+
+   def _interp_dose_along_ax(self,hypsrf,lookup,ax):
+      """ interpolate the dose axis along a given spatial axis
+
+          lookup: a matrix of the shape of spatial dimensions where each elements
+                  corresponds to the dose bin at that point (produced by np.digitize)
+          hypsrfs: the hypersurface: [x,y,z,dose_bin]
+          ax: the index along which the interpolation is done (x=0,y=1,z=2)
+      """
+
+      dims = self.ref_img.shape # the spatial dimensions triplet (x, y, z)
+      hs = np.copy(hypsrf)
+
+      ax_slc = slice(None) # take the whole ax (same as [:])
+      ax_order = [ax] + [x for x in [0,1,2] if x != ax] # [ax, oax1, oax2]
+      ax_sort = np.argsort(ax_order)
+
+      # a function that take (ax, oax1, oax2) and return (x,y,z)
+      xyz = lambda x,y,z : ([x,y,z][ax_sort[0]], [x,y,z][ax_sort[1]], [x,y,z][ax_sort[2]])
+
+      for oax1 in range(dims[ax_order[1]]):
+         for oax2 in range(dims[ax_order[2]]):
+            v = lookup[xyz(ax_slc,oax1,oax2)]
+            jumps = np.diff(v) # number of pixels in gap
+            idx_jump = np.argwhere(np.abs(jumps) > 1).flatten() # next dose jumps by more than 1 bin
+
+            for i in idx_jump:
+               # starting point to pad on hyper surface: (i,v[i])
+               pad = 0
+               if jumps[i] < 0: # dose decrease
+                  pad = int(np.floor(jumps[i]/2)) # divide the gap in 2
+                  hs[xyz(i,oax1,oax2) + (slice((v[i] + pad),v[i]),)] = 0 # pad down
+                  hs[xyz(i+1,oax1,oax2) + (slice(v[i+1],(v[i+1]-pad)),)] = 0 # pad up
+               else: # dose increase
+                  pad = int(np.ceil(jumps[i]/2))
+                  hs[xyz(i,oax1,oax2) + (slice(v[i],(v[i]+pad)),)] = 0 # pad up
+                  hs[xyz(i+1,oax1,oax2) + (slice((v[i+1] - pad),v[i+1]),)] = 0 # pad down
+      return hs
+
+   def compute_gamma(self,img):
+      """ compute gamma between img and ref
+          img must have the same number and size of pixels
+      """
+
+      assert self.ref_img.shape == img.shape, 'reference and test must have the same dimensions'
+
+      lookup = np.digitize(img,self.dbins) - 1 # values to lookup in dist_map
+      gamma_map = np.ones(img.shape)*999 # initialize
+      print(gamma_map.shape)
+
+      #gamma values corrresponds to the pixel values on dist_map at the location of img
+      for i in range(self.ndbins):
+         test_points = lookup == i
+         gamma_map[test_points] = self.dist_map[:,:,:,i][test_points]
+
+      return gamma_map
+
+   def __call__(self,img):
+      """ compute the gamma between ref and img
+      """
+
+      return self.compute_gamma(img)
---
+++ readme.md
@@ -0,0 +1,39 @@
+# Gamma calculation
+
+An implementation of the local $\gamma$ calculation algorithm described by Chen et al:
+
+> Chen, M., Lu, W., Chen, Q., Ruchala, K., & Olivera, G. (2009).
+> Efficient gamma index calculation using fast Euclidean distance transform. Physics in Medicine and Biology, 54(7), 2037–2047.
+> https://doi.org/10.1088/0031-9155/54/7/012
+
+That calculation uses the euclidean distance transform (EDT) on a $k+1$ space composed of $k$ spatial coordinates (currently $k=3$)
+and one dose coordinate. By scaling the the spatial and dose axis with their respective criterion, the gamma value of a point
+$(\mathbf x,d)$ on a testing dose distribution is simply the value at the same location on the EDT of the reference dose distribution.
+
+## Dependencies
+
+- `numpy`
+- `scipy`
+
+## Example of use
+
+Assuming two 3D dose maps: `ref` and `test`
+
+```python
+from gamma3D import gamma
+
+Delta = 3 # [% of max]
+delta = 3 # [mm]
+
+g = gamma(Delta,delta)
+g.set_reference(ref)
+gamma_map = g(test)
+```
+
+## To do:
+- [ ] Accelerate with masked arrays and `skfmm.distance`
+- [ ] Better management of units
+- [ ] allow for sub-sampling or oversampling dose grid within the gamma class
+- [ ] dose threshold to ignore low-dose voxels (might require another librairy such as `skfmm`)
+- [ ] Allow 2D gamma
+- [ ] option for local gamma
