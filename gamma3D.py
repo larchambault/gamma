71f60eebd95a8e58181f39e4a979771c416e9e51
larchambault <36712660+larchambault@users.noreply.github.com>
2019-05-02 18:06:44 -0400

    wip 2D gamma this does not work yet

--- gamma3D.py
+++ gamma3D.py
@@ -7,6 +7,8 @@ Created on 2018-12-06
 import numpy as np
 from scipy.ndimage.morphology import distance_transform_edt as edt
 
+import skfmm # [scikit-fmm](https://github.com/scikit-fmm/scikit-fmm)
+
 class gamma:
    """ Gamma calculation (global) based on Chen et al. 2009
 
@@ -16,11 +18,12 @@ class gamma:
    """
 
 
-   def __init__(self,dose_crit,dist_crit,threshold,vox_size = None,ref=None):
+   def __init__(self,dose_crit,dist_crit,vox_size,threshold = None,ref=None):
       """ initialization
 
           ref: reference dose distribution a 3D numpy array
-          vox size: voxel size [x,y,z] in mm
+          vox_size: voxel size [x,y,z] in mm
+            - Note that image shape is infered from vox_size
           dose_crit: \Delta [percent of max]
           dist_crit: \delta [mm]
           threshold: minimum dose below which gamma value is not computed [percent of max]
@@ -29,10 +32,9 @@ class gamma:
       self.dist_crit = dist_crit
       self.dose_crit = dose_crit
       self.min_threshold = threshold
-      if vox_size is not None:
-         self.set_voxel_size(vox_size)
-      else:
-         self.set_voxel_size([1,1,1])
+      self.set_voxel_size(vox_size)
+      self.ndim = len(self.vox_size)
+      print(f'Image dimensions: {self.ndim}')
       self.max_dose_pct = 120 # [%], fraction of maximum to use as the upper dose bin
 
       # empty initialization, waiting for ref image
@@ -41,7 +43,6 @@ class gamma:
       self.ref_img = None
       self.dist_map = None
       self.delta = None # voxel sizes as fx of criterion
-
 
       if ref is not None:
          self.set_reference(ref)
@@ -60,7 +61,10 @@ class gamma:
       abs_dose_crit = max_dose*self.dose_crit/100.
 
       # absolute threshold criterion
-      self.abs_min_threshold = self.min_threshold/100 * max_dose
+      if self.min_threshold:
+         self.abs_min_threshold = self.min_threshold/100 * max_dose
+      else:
+         self.abs_min_threshold = None
 
       # Set the number of dose bins to match the voxel dose length (i.e. dd)
       # dd = dose_vox_size/abs_dose_crit
@@ -89,22 +93,38 @@ class gamma:
       # Dose points are set to 0
 
       lookup = np.digitize(self.ref_img,self.dbins) - 1 # lookup contains the index of dose bins
+
       for i in range(self.ndbins):
          dose_points = lookup == i
-         hypSurf[:,:,:,i][dose_points] = 0
+         if self.ndim == 3:
+            hypSurf[:,:,:,i][dose_points] = 0
+            # simple (naive) interpolation. See Fig. 2 au Chen 2009
+            hypSurf = self._interp_dose_along_ax3(hypSurf,lookup,0)
+            hypSurf = self._interp_dose_along_ax3(hypSurf,lookup,1)
+            hypSurf = self._interp_dose_along_ax3(hypSurf,lookup,2)
+         elif self.ndim == 2:
+            print('allo')
+            hypSurf[:,:,i][dose_points] = 0
+            # simple (naive) interpolation. See Fig. 2 au Chen 2009
+            hypSurf = self._interp_dose_along_ax2(hypSurf,lookup,0)
+            print('allo2')
+            hypSurf = self._interp_dose_along_ax2(hypSurf,lookup,1)
+            print('allo3')
+         else:
+            raise IndexError('Only 2 and 3 spatial dimension supported at this moment')
 
-      # simple (naive) interpolation. See Fig. 2 au Chen 2009
-      hypSurf = self._interp_dose_along_ax(hypSurf,lookup,0)
-      hypSurf = self._interp_dose_along_ax(hypSurf,lookup,1)
-      hypSurf = self._interp_dose_along_ax(hypSurf,lookup,2)
+      # DEBUG
+      # make sure hypsurf is correctly interpolated in 2D
+      self.hyp = hypSurf
+      return 0
 
       # print(self.delta)
       dst = edt(hypSurf,sampling=self.delta)
 
       self.dist_map = dst
 
-   def _interp_dose_along_ax(self,hypsrf,lookup,ax):
-      """ interpolate the dose axis along a given spatial axis
+   def _interp_dose_along_ax3(self,hypsrf,lookup,ax):
+      """ interpolate the dose axis along a given spatial axis (3D)
 
           lookup: a matrix of the shape of spatial dimensions where each elements
                   corresponds to the dose bin at that point (produced by np.digitize)
@@ -139,6 +159,44 @@ class gamma:
                   pad = int(np.ceil(jumps[i]/2))
                   hs[xyz(i,oax1,oax2) + (slice(v[i],(v[i]+pad)),)] = 0 # pad up
                   hs[xyz(i+1,oax1,oax2) + (slice((v[i+1] - pad),v[i+1]),)] = 0 # pad down
+      return hs
+
+   def _interp_dose_along_ax2(self,hypsrf,lookup,ax):
+      """ interpolate the dose axis along a given spatial axis (2D)
+
+          lookup: a matrix of the shape of spatial dimensions where each elements
+                  corresponds to the dose bin at that point (produced by np.digitize)
+          hypsrfs: the hypersurface: [x,y,z,dose_bin]
+          ax: the index along which the interpolation is done (x=0,y=1,z=2)
+      """
+
+      dims = self.ref_img.shape # the spatial dimensions triplet (x, y, z)
+      hs = np.copy(hypsrf)
+
+      ax_slc = slice(None) # take the whole ax (same as [:])
+      ax_order = [ax] + [x for x in [0,1] if x != ax] # [ax, oax1]
+      ax_sort = np.argsort(ax_order)
+
+      # a function that take (ax, oax1, oax2) and return (x,y,z)
+      xy = lambda x,y : ([x,y][ax_sort[0]], [x,y][ax_sort[1]])
+
+      for oax1 in range(dims[ax_order[1]]):
+         #for oax2 in range(dims[ax_order[2]]):
+         v = lookup[xy(ax_slc,oax1)]
+         jumps = np.diff(v) # number of pixels in gap
+         idx_jump = np.argwhere(np.abs(jumps) > 1).flatten() # next dose jumps by more than 1 bin
+
+         for i in idx_jump:
+            # starting point to pad on hyper surface: (i,v[i])
+            pad = 0
+            if jumps[i] < 0: # dose decrease
+               pad = int(np.floor(jumps[i]/2)) # divide the gap in 2
+               hs[xy(i,oax1) + (slice((v[i] + pad),v[i]),)] = 0 # pad down
+               hs[xy(i+1,oax1) + (slice(v[i+1],(v[i+1]-pad)),)] = 0 # pad up
+            else: # dose increase
+               pad = int(np.ceil(jumps[i]/2))
+               hs[xy(i,oax1) + (slice(v[i],(v[i]+pad)),)] = 0 # pad up
+               hs[xy(i+1,oax1) + (slice((v[i+1] - pad),v[i+1]),)] = 0 # pad down
       return hs
 
    def compute_gamma(self,img):
